f30d48d88cd6d3850ac8b0263614a7a620913c4b
MaShengnan <msn2047679424@163.com>
2025-08-14 10:28:55 +0800

    新增main文件中的输入语句

---
+++ main.py
@@ -0,0 +1,11 @@
+from digit_config.multi import multi
+from digit_config.sum import sum_num
+
+
+
+if __name__=='main.py':
+    a,b=1,2
+    sum_1=sum_num(a,b)
+    multi_1=multi(a,b)
+    print(sum_1,multi_1)
+    print("hello world")
